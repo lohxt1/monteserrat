f7c5195262993736e80d154ef37c537d98e5acb6
lohith warlock <lohxt.space@gmail.com>
2023-03-04 21:43:14 +0530

    feat: added fathom

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,31 @@
 // import "@/styles/globals.css";
 import { Montserrat } from "@next/font/google";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import * as Fathom from "fathom-client";
 
 const monteserrat = Montserrat({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    Fathom.load("QXSMHHMR", {
+      includedDomains: ["montserrat.lohxt.space"],
+    });
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+    }
+    // Record a pageview when route changes
+    router.events.on("routeChangeComplete", onRouteChangeComplete);
+
+    // Unassign event listener
+    return () => {
+      router.events.off("routeChangeComplete", onRouteChangeComplete);
+    };
+  }, []);
+
   return (
     <>
       <Component {...pageProps} />
